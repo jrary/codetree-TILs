1cd4105cd48007b03c8b4e501cd77d1aa62438e3
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-07-03 13:12:01 +0900

    Title: 최대로 겹치는 구간, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240703/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240703/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,8 +1,24 @@
 import sys
 N = int(sys.stdin.readline())
-line = list(0 for _ in range(100))
+
+check = []
+min_start = 0
+max_end = 0
 for _ in range(N):
     a, b = map(int, sys.stdin.readline().split())
+    check.append([a, b])
+    if a < min_start:
+        min_start = a
+    if b > max_end:
+        max_end = b
+
+# 모든 좌표에 대해 특정 offset(min_start: 최소숫자)을 더한다.
+for i in range(N):
+    check[i][0] -= min_start
+    check[i][1] -= min_start
+
+line = list(0 for _ in range(max_end - min_start))
+for a, b in check:
     for i in range(a, b):
         line[i] += 1
 

0016fa1c8e318697d22951fe1836f25619d362e9
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-07-14 17:09:11 +0900

    Title: 1이 3개 이상 있는 위치, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240714/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,30 @@
+import sys
+N = int(sys.stdin.readline())
+li = []
+for _ in range(N):
+    li.append(list(map(int, sys.stdin.readline().split())))
+
+d = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+]
+
+def find(x, y):
+    now_res = 0
+    for dx, dy in d:
+        nx = x+dx
+        ny = y+dy
+        if nx < N and ny < N and li[nx][ny] == 1:
+            now_res += 1
+    return now_res
+
+result = 0
+for i in range(N):
+    for j in range(N):
+        now_res = find(i, j)
+        if now_res >= 3:
+            result += 1
+
+print(result)

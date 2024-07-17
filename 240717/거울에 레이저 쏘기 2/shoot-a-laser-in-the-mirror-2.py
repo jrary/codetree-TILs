d1072d97e35d477c52008bea929bab63e19c58aa
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-07-17 13:00:16 +0900

    Title: 거울에 레이저 쏘기 2, Time: 162ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240717/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,58 @@
+import sys
+N = int(sys.stdin.readline())
+mirror = []
+for _ in range(N):
+    mirror.append(list(sys.stdin.readline().strip()))
+K = int(sys.stdin.readline())
+
+d = [
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+    [0, 1]
+]
+
+K -= 1
+a = K // N
+b = K % N
+x, y = 0, 0
+
+di = a
+if a == 0:
+    x = 0
+    y = b
+elif a == 1:
+    x = b
+    y = N - 1
+elif a == 2:
+    x = N - 1
+    y = N - b - 1
+elif a == 3:
+    x = N - b - 1
+    y = 0
+
+count = 0
+while x < N and x >= 0 and y < N and y >= 0:
+    count += 1
+    if mirror[x][y] == '\\':
+        if di == 0:
+            di = 3
+        elif di == 1:
+            di = 2
+        elif di == 2:
+            di = 1
+        elif di == 3:
+            di = 0
+    elif mirror[x][y] == '/':
+        if di == 0:
+            di = 1
+        elif di == 1:
+            di = 0
+        elif di == 2:
+            di = 3
+        elif di == 3:
+            di = 2
+    x += d[di][0]
+    y += d[di][1]
+
+print(count)

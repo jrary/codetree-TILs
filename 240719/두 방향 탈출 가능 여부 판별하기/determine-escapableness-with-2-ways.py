3b65eda57209bbd6990b807a94cbac30da6ee097
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-07-19 11:46:10 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,6 +1,7 @@
 import sys
 N, M = map(int, sys.stdin.readline().split())
 route = []
+visited = [[0 for _ in range(M)] for _ in range(N)]
 for _ in range(N):
     route.append(list(map(int, sys.stdin.readline().split())))
 d = [
@@ -9,12 +10,13 @@ d = [
 ]
 
 def dfs(a, b):
+    visited[a][b] = 1
     if a == N-1 and b == M-1:
         return 1
     for i in range(2):
         na = a + d[i][0]
         nb = b + d[i][1]
-        if na < N and nb < M and route[na][nb] == 1:
+        if na < N and nb < M and route[na][nb] == 1 and visited[na][nb] == 0:
             return dfs(na, nb)
 
 print(1 if dfs(0, 0) else 0)

b3c8bfbfdc564cab2efe1b3fc00f11610f86987f
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-07-19 21:54:22 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240719/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240719/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -12,21 +12,19 @@ di = [
 ]
 
 visited = [[0 for _ in range(M)] for _ in range(N)]
-result = 0
 def bfs():
-    global result
     queue = deque()
     queue.append([0, 0])
     while queue:
         x, y = queue.popleft()
         visited[x][y] = 1
         if x == N - 1 and y == M - 1:
-            result = 1
+            return 1
         for dx, dy in di:
             nx = x + dx
             ny = y + dy
             if nx >= 0 and nx < N and ny >= 0 and ny < M and visited[nx][ny] == 0 and snake[nx][ny] == 1:
                 queue.append([nx, ny])
 
-bfs()
-print(result)
+
+print(bfs())

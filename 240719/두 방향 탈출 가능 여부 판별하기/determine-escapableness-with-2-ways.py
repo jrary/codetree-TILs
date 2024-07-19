602d7847fc44e6608c8970cf754a4991cb82f66a
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-07-19 20:29:25 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 167ms, Memory: 27MB, Status: Passed - CodeTree

--- 240719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,28 +1,24 @@
 import sys
-sys.setrecursionlimit(10000)  # 재귀 한도를 늘립니다.
-
 N, M = map(int, sys.stdin.readline().split())
 route = []
+visited = [[0 for _ in range(M)] for _ in range(N)]
 for _ in range(N):
     route.append(list(map(int, sys.stdin.readline().split())))
-
 d = [
     [0, 1],
     [1, 0]
 ]
-
-def dfs(a, b, visited):
+result = 0
+def dfs(a, b):
+    global result
+    visited[a][b] = 1
     if a == N-1 and b == M-1:
-        return True
-    visited[a][b] = True
+        result = 1
     for i in range(2):
         na = a + d[i][0]
         nb = b + d[i][1]
-        if 0 <= na < N and 0 <= nb < M and route[na][nb] == 1 and not visited[na][nb]:
-            if dfs(na, nb, visited):
-                return True
-    visited[a][b] = False  # 탐색을 위해 다시 False로 바꿉니다.
-    return False
+        if na < N and nb < M and route[na][nb] == 1 and visited[na][nb] == 0:
+            dfs(na, nb)
 
-visited = [[False] * M for _ in range(N)]
-print(1 if dfs(0, 0, visited) else 0)
+dfs(0, 0)
+print(result)

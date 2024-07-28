84a91e2b156ff4259bd9be897e214b3281b9fd71
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-07-28 23:38:24 +0900

    Title: 우리는 하나, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240728/우리는 하나/we-are-the-one.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+N, K, U, D = map(int, sys.stdin.readline().split())
+building = []
+for _ in range(N):
+    building.append(list(map(int, sys.stdin.readline().split())))
+di = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+]
+
+visited = [[0 for _ in range(N)] for _ in range(N)]
+def bfs(a, b):
+    queue = deque()
+    queue.append([a, b])
+    visited[a][b] = 1
+    count = 0
+    while queue:
+        x, y = queue.popleft()
+        count += 1
+        for dx, dy in di:
+            nx = x + dx
+            ny = y + dy
+            if nx >= 0 and nx < N and ny >= 0 and ny < N and visited[nx][ny] == 0:
+                height_abs = abs(building[x][y] - building[nx][ny])
+                if height_abs >= U and height_abs <= D:
+                    visited[nx][ny] = 1
+                    queue.append([nx, ny])
+    return count
+
+result = []
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] == 0:
+            result.append(bfs(i, j))
+
+result.sort(reverse=True)
+res = 0
+for i in range(K):
+    res += result[i]
+
+print(res)

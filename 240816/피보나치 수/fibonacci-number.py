f87c201e303ac5987cc723cf5d765e2f004ab4d0
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-08-16 20:11:14 +0900

    Title: 피보나치 수, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240816/피보나치 수/fibonacci-number.py
@@ -0,0 +1,10 @@
+import sys
+N = int(sys.stdin.readline())
+fibo = []
+fibo.append(0)
+fibo.append(1)
+def dp():
+    for i in range(N):
+        fibo.append(fibo[i] + fibo[i+1])
+
+print(fibo[N-1])
